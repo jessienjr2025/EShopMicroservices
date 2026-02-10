3f865a3cfca75c2290100a941d4aa4b75134cca1
jessienjr22 <jessie.nightingale@va.gov>
2026-02-10 16:06:25 -0500

    Completed Catalog API with Docker

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,47 @@
+
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service to the container 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssemblies(assembly);
+	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+	config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+	opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+// Only seed the database in Local DEV
+if (builder.Environment.IsDevelopment())
+	builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+	.AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.MapCarter();
+
+// Global Exception Handling
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+	new HealthCheckOptions
+	{
+		ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse 
+	});
+
+app.Run();
